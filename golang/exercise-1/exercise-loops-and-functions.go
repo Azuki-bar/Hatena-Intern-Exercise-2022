bfdca4a0f9e2df2078ad399a44bcbbde1778e41c
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-08-11 23:48:19 +0900

    go clear exercise-1

--- golang/exercise-1/exercise-loops-and-functions.go
+++ golang/exercise-1/exercise-loops-and-functions.go
@@ -5,6 +5,20 @@ import (
 )
 
 func Sqrt(x float64) float64 {
+	z := 1.0
+	count := 1
+	allowedRange := 0.00001
+	for {
+		dz := (z*z - x) / (2 * z)
+		z -= dz
+		if -allowedRange <= dz && dz <= allowedRange {
+			return z
+		}
+		count++
+		if count%10 == 0 {
+			fmt.Println(z)
+		}
+	}
 }
 
 func main() {
