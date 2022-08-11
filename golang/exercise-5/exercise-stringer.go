925677d834dc1b2b508274e8e5c248eb4fe4975f
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-08-12 04:20:09 +0900

    clear go exercise 5

--- golang/exercise-5/exercise-stringer.go
+++ golang/exercise-5/exercise-stringer.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type IPAddr [4]byte
 
 // TODO: Add a "String() string" method to IPAddr.
-
+// https://github.com/golang/go/blob/62654dfd4f8d7ac2309232dc3c09c33b1741ce95/src/net/ip.go#L313
+func (ip IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3])
+}
 func main() {
 	hosts := map[string]IPAddr{
 		"loopback":  {127, 0, 0, 1},
