2e4c9bbea9713145b6ebbe66f4253bbc84e99876
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-08-12 05:34:48 +0900

    clear go exercise 9!

--- golang/exercise-9/exercise-images.go
+++ golang/exercise-9/exercise-images.go
@@ -1,9 +1,25 @@
 package main
 
-import "golang.org/x/tour/pic"
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
+
+const (
+	width  = 255
+	height = 255
+)
 
 type Image struct{}
 
+func (Image) ColorModel() color.Model { return color.RGBAModel }
+func (Image) Bounds() image.Rectangle { return image.Rect(0, 0, width, height) }
+func (Image) At(x, y int) color.Color {
+	v := uint8(x*x + y*y)
+	return color.RGBA{v, v, 255, 255}
+}
 func main() {
 	m := Image{}
 	pic.ShowImage(m)
