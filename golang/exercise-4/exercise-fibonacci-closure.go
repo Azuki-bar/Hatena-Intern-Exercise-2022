93c330b70d3d46f5e51f5e8592726804900e2af6
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-08-12 03:54:27 +0900

    clear go exercise 4

--- golang/exercise-4/exercise-fibonacci-closure.go
+++ golang/exercise-4/exercise-fibonacci-closure.go
@@ -5,6 +5,21 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
+	a1, a2 := 0, 1
+	count := 0
+	return func() int {
+		defer func() { count++ }()
+		switch count {
+		case 0:
+			return 0
+		case 1:
+			return 1
+		default:
+			fib := a1 + a2
+			a1, a2 = a2, fib
+			return fib
+		}
+	}
 }
 
 func main() {
