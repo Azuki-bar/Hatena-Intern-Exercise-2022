fa145cc1fbf38c459abf27dc89e437aa264d397b
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-08-12 04:28:33 +0900

    clear go exercise 6

--- golang/exercise-6/exercise-errors.go
+++ golang/exercise-6/exercise-errors.go
@@ -4,8 +4,29 @@ import (
 	"fmt"
 )
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %.0f", float64(e))
+}
 func Sqrt(x float64) (float64, error) {
-	return 0, nil
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	z := 1.0
+	count := 1
+	allowedRange := 0.00001
+	for {
+		dz := (z*z - x) / (2 * z)
+		z -= dz
+		if -allowedRange <= dz && dz <= allowedRange {
+			return z, nil
+		}
+		count++
+		if count%10 == 0 {
+			fmt.Println(z)
+		}
+	}
 }
 
 func main() {
