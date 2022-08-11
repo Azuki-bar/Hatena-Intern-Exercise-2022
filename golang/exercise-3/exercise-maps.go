96af0044bcb92c839dc30afca56d227909d6403d
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-08-12 03:36:08 +0900

    clear go exercise3

--- golang/exercise-3/exercise-maps.go
+++ golang/exercise-3/exercise-maps.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"strings"
+
 	"golang.org/x/tour/wc"
 )
 
 func WordCount(s string) map[string]int {
-	return map[string]int{"x": 1}
+	words := strings.Fields(s)
+	count := make(map[string]int)
+	for _, word := range words {
+		count[word] += 1
+		// if c, ok := count[word]; ok {
+		// 	count[word] = c + 1
+		// } else {
+		// 	count[word] = 1
+		// }
+	}
+	return count
 }
 
 func main() {
