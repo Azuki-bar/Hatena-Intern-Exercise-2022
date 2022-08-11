e86ebf3bb9e1f17bcf5a6f5cf037f49084541af8
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-08-12 05:23:19 +0900

    clear go exercise 8

--- golang/exercise-8/exercise-rot-reader.go
+++ golang/exercise-8/exercise-rot-reader.go
@@ -10,6 +10,28 @@ type rot13Reader struct {
 	r io.Reader
 }
 
+func (rot rot13Reader) Read(b []byte) (n int, err error) {
+	buf := make([]byte, len(b))
+	n, err = rot.r.Read(buf)
+	if err != nil {
+		return n, err
+	}
+	var i int
+	for i = 0; i < n; i++ {
+		elem := buf[i]
+		switch {
+		case 'A' <= elem && elem <= 'Z':
+			b[i] = (elem-'A'+13)%26 + 'A'
+		case 'a' <= elem && elem <= 'z':
+			b[i] = (elem-'a'+13)%26 + 'a'
+		case elem == 0:
+			return n, io.EOF
+		default:
+			b[i] = elem
+		}
+	}
+	return i, nil
+}
 func main() {
 	s := strings.NewReader("Lbh penpxrq gur pbqr!")
 	r := rot13Reader{s}
