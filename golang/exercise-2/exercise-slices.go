99f74012ed92493b784537fa3b1f0cd7c38e394c
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-08-12 00:13:04 +0900

    go exercise 2 done

--- golang/exercise-2/exercise-slices.go
+++ golang/exercise-2/exercise-slices.go
@@ -2,7 +2,19 @@ package main
 
 import "golang.org/x/tour/pic"
 
+func imageVal(x, y int) int {
+	return x*x + y*y
+}
+
 func Pic(dx, dy int) [][]uint8 {
+	var img [][]uint8
+	for x := 0; x < dx; x++ {
+		img = append(img, make([]uint8, dy))
+		for y := 0; y < dy; y++ {
+			img[x][y] = uint8(imageVal(x, y))
+		}
+	}
+	return img
 }
 
 func main() {
