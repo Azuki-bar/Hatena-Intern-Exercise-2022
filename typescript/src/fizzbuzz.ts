94a193a64c15c26acb5fc395d624ce792dd928fe
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2022-08-18 16:02:26 +0900

    implement fizzbuzz

--- typescript/src/fizzbuzz.ts
+++ typescript/src/fizzbuzz.ts
@@ -3,6 +3,33 @@ export function initializeFizzBuzz(
   button: HTMLButtonElement
 ): void {
   console.debug("initializeFizzBuzz", list, button);
-
   // Implement Me!
+  const fizzbuzz = new FizzBuzz();
+  button.addEventListener('click', () => {
+    const newElem = document.createElement('li')
+    newElem.textContent = fizzbuzz.next()
+    list.appendChild(newElem)
+  })
+}
+
+interface IncrementValueValue {
+  next(): string;
+}
+
+class FizzBuzz implements IncrementValueValue {
+  private now: number;
+  constructor() {
+    this.now = 0;
+  }
+  next(): string {
+    this.now++;
+    if (this.now % 15 == 0) {
+      return "FizzBuzz";
+    } else if (this.now % 3 == 0) {
+      return "Fizz";
+    } else if (this.now % 5 == 0) {
+      return "Buzz";
+    }
+    return this.now.toString();
+  }
 }
